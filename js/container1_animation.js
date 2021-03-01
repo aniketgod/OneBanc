c3d9909bb5e11b5d6bc4fa6b7a93748b380a74df
aniketgod <aniketallform@gmail.com>
2021-03-01 09:50:51 +0530

    animationsmooth

--- js/container1_animation.js
+++ js/container1_animation.js
@@ -1,5 +1,13 @@
 var data=["REWARDS", "SAVINGS" , "INVESTMENTS", "FOREX", "ANALAYTICS", "CARDS" , "PAYMENTS", "LOAN", "SUPPORT"]
 
+var datax=["REWARDSX", "SAVINGSX" , "INVESTMENTSX", "FOREXX", "ANALYTICSX", "CARDSX" , "PAYMENTSX", "LOANX", "SUPPORTX"]
+var datay=["REWARDSY", "SAVINGSY" , "INVESTMENTSY", "FOREXY", "ANALYTICSY", "CARDSY" , "PAYMENTSY", "LOANY", "SUPPORTY"]
+var small=["icon/i_rewards.png", "icon/i_saving.png","icon/i_investment.png","icon/i_forex.png","icon/i_Analytics.png", "icon/i_card.png", "icon/i_payment.png", "icon/i_loan.png", "icon/i_support.png"];
+var large=["icon/i_rewardsz.png", "icon/i_savingz.png","icon/i_investmentz.png","icon/i_forexz.png","icon/i_Analyticsz.png", "icon/i_cardz.png", "icon/i_paymentz.png", "icon/i_loanz.png", "icon/i_supportz.png"]
+
+
+var y1=0;
+var y2=0;
 
 var myVar = setInterval(myTimer, 100);
 var i=0;
@@ -7,6 +15,7 @@ var j=0;
 var k=true;
 var p=false;
 var q="";
+var count=10;
 function myTimer() {
   if(i==data.length)
   {
@@ -50,5 +59,33 @@ document.getElementById('container1_span').innerHTML=(s.slice(0,j+1)+q);
          p=true;
      }
  }
-
+if(count==0)
+{
+    if(y1==datax.length)
+    {
+        y1=0;
+    }
+    document.getElementById(datax[y1]).src = large[y1]; 
+    document.getElementById(datay[y1]).src = large[y1];
+ 
+    for(y2=0; y2<datax.length; y2++)
+    {
+        if(y2!=y1)
+        {
+          document.getElementById(datax[y2]).src = small[y2];
+          document.getElementById(datay[y2]).src = small[y2];
+         
+        }
+    }
+  
+   y1++;
+   count=10;
 }
+ count--;
+}
+
+
+
+
+//
+
